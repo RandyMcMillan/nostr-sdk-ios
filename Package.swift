641e62796fb0891ab14ed5dcf913b4e812d2245f
klabo <joelklabo@gmail.com>
2023-05-23 21:17:13 -0700

    docs: deploy to github pages (#12)

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         // MPLEX Muxer Module
         .package(url: "https://github.com/swift-libp2p/swift-libp2p-mplex", .upToNextMinor(from: "0.1.0")),
         //.package(url: "https://github.com/nostr-sdk/nostr-sdk-ios.git", .upToNextMajor(from: "0.1.0")),
-        .package(url: "https://github.com/realm/SwiftLint.git", .upToNextMinor(from: "0.52.2"))
+        .package(url: "https://github.com/realm/SwiftLint.git", .upToNextMinor(from: "0.52.2")),
+        .package(url: "https://github.com/apple/swift-docc-plugin.git", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
