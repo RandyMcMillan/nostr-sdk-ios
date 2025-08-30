762983e864794c40b3d2933490bbef6326135dd4
randymcmillan <randymcmillan@protonmail.com>
2025-08-30 17:30:26 -0400

    NostrSDKDemo/Helpers/Helpers.swift:dummy keys

--- demo/NostrSDKDemo/Helpers/Helpers.swift
+++ demo/NostrSDKDemo/Helpers/Helpers.swift
@@ -24,20 +24,20 @@ struct DemoHelper {
         return Binding.constant(relay)
     }
     static var validNpub: Binding<String> {
-        Binding.constant("npub1n9rljevamqxrdqjq9dsj74z8u2pynxtlkdcf2qxr9fv9avyhwdqqf6w3at")
+        Binding.constant("npub15d9enu3v0yxyud4jk0pvxk3kmvrzymjpc6f0eq4ck44vr32qck7smrxq6k")
     }
     /// The Nostr SDK project and its maintainers take no responsibility of events signed with this private key which has been open sourced.
     /// Its purpose is for only testing and demos.
     static var validNsec: Binding<String> {
-        Binding.constant("nsec163p74rxf58ndvav7ck8axx39qmt6dvwjgm8z98ckanenzf3mpjyq6875fz")
+        Binding.constant("nsec1uwcvgs5clswpfxhm7nyfjmaeysn6us0yvjdexn9yjkv3k7zjhp2sv7rt36")
     }
     static var validHexPublicKey: Binding<String> {
-        Binding.constant("9947f9659dd80c3682402b612f5447e28249997fb3709500c32a585eb0977340")
+        Binding.constant("a34b99f22c790c4e36b2b3c2c35a36db06226e41c692fc82b8b56ac1c540c5bd")
     }
     /// The Nostr SDK project and its maintainers take no responsibility of events signed with this private key which has been open sourced.
     /// Its purpose is for only testing and demos.
     static var validHexPrivateKey: Binding<String> {
-        Binding.constant("d443ea8cc9a1e6d6759ec58fd31a2506d7a6b1d246ce229f16ecf331263b0c88")
+        Binding.constant("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855")
     }
     static var invalidKey: Binding<String> {
         Binding.constant("not-valid")
