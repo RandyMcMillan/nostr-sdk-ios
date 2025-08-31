77e3508a922a966739603620c4eb5e96d30279c6
randymcmillan <randymcmillan@protonmail.com>
2025-08-31 17:31:24 -0400

    demo/NostrSDKDemo/ContentView.swift

--- demo/NostrSDKDemo/ContentView.swift
+++ demo/NostrSDKDemo/ContentView.swift
@@ -16,6 +16,43 @@ struct ContentView: View {
         NavigationView {
             VStack {
                 List {
+
+                    ListOptionView(destinationView: AnyView(_30618QueryRelayDemoView()),
+                                   imageName: "list.bullet.rectangle.portrait",
+                                   labelText: "NIP-0034 (30618)")
+                    
+                    ListOptionView(destinationView: AnyView(_30617QueryRelayDemoView()),
+                                   imageName: "list.bullet.rectangle.portrait",
+                                   labelText: "NIP-0034 (30617)")
+                    
+                    ListOptionView(destinationView: AnyView(_1633QueryRelayDemoView()),
+                                   imageName: "list.bullet.rectangle.portrait",
+                                   labelText: "NIP-0034 (1633)")
+
+                    ListOptionView(destinationView: AnyView(_1632QueryRelayDemoView()),
+                                   imageName: "list.bullet.rectangle.portrait",
+                                   labelText: "NIP-0034 (1632)")
+
+                    ListOptionView(destinationView: AnyView(_1631QueryRelayDemoView()),
+                                   imageName: "list.bullet.rectangle.portrait",
+                                   labelText: "NIP-0034 (1631)")
+
+                    ListOptionView(destinationView: AnyView(_1630QueryRelayDemoView()),
+                                   imageName: "list.bullet.rectangle.portrait",
+                                   labelText: "NIP-0034 (1630)")
+
+                    ListOptionView(destinationView: AnyView(_1621QueryRelayDemoView()),
+                                   imageName: "list.bullet.rectangle.portrait",
+                                   labelText: "NIP-0034 (1621)")
+
+                    ListOptionView(destinationView: AnyView(_1617QueryRelayDemoView()),
+                                   imageName: "list.bullet.rectangle.portrait",
+                                   labelText: "NIP-0034 (1617)")
+
+                    ListOptionView(destinationView: AnyView(_1632QueryRelayDemoView()),
+                                   imageName: "list.bullet.rectangle.portrait",
+                                   labelText: "NIP-0034 (1632)")
+
                     ListOptionView(destinationView: AnyView(ConnectRelayView(relay: $relay)),
                                    imageName: "network",
                                    labelText: "Connect Relay")
