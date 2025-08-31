b7539e16793f5b2379ab05b6ab1bb37354279191
randymcmillan <randymcmillan@protonmail.com>
2025-08-30 21:31:21 -0400

    demo

---
+++ demo/NostrSDKDemo/Demo_Views/QueryRelayDemoView.swift
@@ -0,0 +1,225 @@
+//
+//  QueryRelayDemoView.swift
+//  NostrSDKDemo
+//
+//  Created by Joel Klabo on 6/15/23.
+//
+
+import SwiftUI
+import NostrSDK
+import Combine
+
+struct QueryRelayDemoView: View {
+
+    @EnvironmentObject var relayPool: RelayPool
+
+    @State private var authorPubkey: String = ""
+    @State private var events: [NostrEvent] = []
+    @State private var eventsCancellable: AnyCancellable?
+    @State private var errorString: String?
+    @State private var subscriptionId: String?
+//30617 30618 1617 1621 1630 1631 1632 1633
+    private let kindOptions = [
+        //0: "Set Metadata",
+        //1: "Text Note",
+        //3: "Follow List",
+        //6: "Repost",
+        //7: "Reaction",
+        //1984: "Report",
+        //10000: "Mute List",
+        //10003: "Bookmarks List",
+        //30023: "Longform Content",
+        
+        //nip-0034
+        
+        30617: "Repository announcements",
+        30618: "Repository state announcements",
+        1617: "Patches",
+        1621: "Issues",
+        1630: "Status (Open)",
+        1631: "Status (Applied / Merged)",
+        1632: "Status (Closed)",
+        1633: "Status (Draft)"
+
+    ]
+
+    @State private var selectedKind = 30617
+
+    var body: some View {
+        
+        //NavigationView {
+        //    VStack {
+        //        List {
+        //            ListOptionView(destinationView: AnyView(RelaysView()),
+        //                           imageName: "network",
+        //                           labelText: "Configure Relays")
+        //            ListOptionView(destinationView: AnyView(QueryRelayDemoView()),
+        //                           imageName: "list.bullet.rectangle.portrait",
+        //                           labelText: "NIP-0034 Viewer")
+        //            ListOptionView(destinationView:
+        //                            AnyView(LegacyDirectMessageDemoView()),
+        //                           imageName: "list.bullet",
+        //                           labelText: "NIP-04 Direct Message")
+        //            ListOptionView(destinationView:
+        //                            AnyView(EncryptMessageDemoView()),
+        //                           imageName: "list.bullet",
+        //                           labelText: "NIP-44 Encrypt")
+        //            ListOptionView(destinationView:
+        //                            AnyView(DecryptMessageDemoView()),
+        //                           imageName: "list.bullet",
+        //                           labelText: "NIP-44 Decrypt")
+        //            ListOptionView(destinationView: AnyView(GenerateKeyDemoView()),
+        //                           imageName: "key",
+        //                           labelText: "Key Generation")
+        //            ListOptionView(destinationView: AnyView(NIP05VerficationDemoView()),
+        //                           imageName: "checkmark.seal",
+        //                           labelText: "NIP-05")
+        //        }
+        //    }
+        //    .navigationTitle("NIP-0034 Viewer")
+        //    .navigationBarTitleDisplayMode(.inline)
+        //}
+    //}
+
+        
+        Form {
+            Section("NIP-0034 Viewer") {
+
+                TextField(text: $authorPubkey) {
+                    Text("Author Public Key (HEX)")
+                }
+
+                Picker("Kind", selection: $selectedKind) {
+                    ForEach(kindOptions.keys.sorted(), id: \.self) { number in
+                        if let name = kindOptions[number] {
+                            Text("\(name) (\(String(number)))")
+                        } else {
+                            Text("\(String(number))")
+                        }
+                    }
+                }
+            }
+
+            Button {
+                updateSubscription()
+            } label: {
+                Text("Query")
+            }
+
+            if !events.isEmpty {
+                Section("Results") {
+                    if !authorPubkey.isEmpty {
+                        Text("Note: send an event from this account and see it appear here.")
+                            .foregroundColor(.gray)
+                            .font(.footnote)
+                    }
+                    //
+                    
+                    
+                    NavigationView {
+                        VStack {
+                            //List {
+                            //    ListOptionView(destinationView: AnyView(RelaysView()),
+                            //                   imageName: "network",
+                            //                   labelText: "Configure Relays")
+                            //    ListOptionView(destinationView: AnyView(QueryRelayDemoView()),
+                            //                   imageName: "list.bullet.rectangle.portrait",
+                            //                   labelText: "NIP-0034 Viewer")
+                            //    ListOptionView(destinationView:
+                            //                    AnyView(LegacyDirectMessageDemoView()),
+                            //                   imageName: "list.bullet",
+                            //                   labelText: "NIP-04 Direct Message")
+                            //    ListOptionView(destinationView:
+                            //                    AnyView(EncryptMessageDemoView()),
+                            //                   imageName: "list.bullet",
+                            //                   labelText: "NIP-44 Encrypt")
+                            //    ListOptionView(destinationView:
+                            //                    AnyView(DecryptMessageDemoView()),
+                            //                   imageName: "list.bullet",
+                            //                   labelText: "NIP-44 Decrypt")
+                            //    ListOptionView(destinationView: AnyView(GenerateKeyDemoView()),
+                            //                   imageName: "key",
+                            //                   labelText: "Key Generation")
+                            //    ListOptionView(destinationView: AnyView(NIP05VerficationDemoView()),
+                            //                   imageName: "checkmark.seal",
+                            //                   labelText: "NIP-05")
+                            //}
+                       // }
+                       // .navigationTitle("NIP-0034 Viewer")
+                       // .navigationBarTitleDisplayMode(.inline)
+                    //}
+                    List(events, id: \.id) { event in
+                        if !event.content.isEmpty {
+                            Text("")
+                            Text("event.id \(event.id)")
+                            Text("event.kind \(event.kind)")
+                            Text("event.tags \(event.tags)")
+                            Text("event.pubkey \(event.pubkey)")
+                            Text("")
+                            Text("\(event.content)")
+                        } else {
+                            //Text("Empty content field for event \(event.id)")
+                            Text("")
+                            Text("event.id \(event.id)")
+                            Text("event.kind \(event.kind)")
+                            Text("event.tags \(event.tags)")
+                            Text("event.pubkey \(event.pubkey)")
+                            Text("")
+                        }
+                    }
+                        }
+                    }
+                }
+            }
+        }
+        .onChange(of: authorPubkey) { _ in
+            events = []
+            updateSubscription()
+        }
+        .onChange(of: selectedKind) { _ in
+            events = []
+            updateSubscription()
+        }
+        .onDisappear {
+            if let subscriptionId {
+                relayPool.closeSubscription(with: subscriptionId)
+            }
+        }
+    }
+    
+    private var currentFilter: Filter {
+        let authors: [String]?
+        if authorPubkey.isEmpty {
+            authors = nil
+        } else {
+            authors = [authorPubkey]
+        }
+        return Filter(authors: authors, kinds: [selectedKind])!//
+    }
+    
+    private func updateSubscription() {
+        if let subscriptionId {
+            relayPool.closeSubscription(with: subscriptionId)
+        }
+        
+        subscriptionId = relayPool.subscribe(with: currentFilter)
+        
+        eventsCancellable = relayPool.events
+            .receive(on: DispatchQueue.main)
+            .map {
+                $0.event
+            }
+            .removeDuplicates()
+            .sink { event in
+                events.insert(event, at: 0)
+            }
+    }
+}
+
+struct QueryRelayView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            QueryRelayDemoView()
+        }
+    }
+}
