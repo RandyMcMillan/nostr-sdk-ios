027dc81f8884fb1489388d6d0834b2d07ecc5102
randymcmillan <randymcmillan@protonmail.com>
2025-08-31 20:54:33 -0400

    ContentView.swift

--- demo/NostrSDKDemo/ContentView.swift
+++ demo/NostrSDKDemo/ContentView.swift
@@ -17,68 +17,80 @@ struct ContentView: View {
             VStack {
                 List {
 
+                    // ListOptionView(destinationView: AnyView(_30618QueryRelayDemoView()),
+                    //               imageName: "list.bullet.rectangle.portrait",
+                    //               labelText: "NIP-0034 (30618)")
+
+                    // Assuming you have an image file named "network" in your Assets.xcassets
                     ListOptionView(destinationView: AnyView(_30618QueryRelayDemoView()),
-                                   imageName: "list.bullet.rectangle.portrait",
+                                   customImageName: "network",
                                    labelText: "NIP-0034 (30618)")
 
                     ListOptionView(destinationView: AnyView(_30617QueryRelayDemoView()),
-                                   imageName: "list.bullet.rectangle.portrait",
+                                   customImageName: "network",
                                    labelText: "NIP-0034 (30617)")
 
                     ListOptionView(destinationView: AnyView(_1633QueryRelayDemoView()),
-                                   imageName: "list.bullet.rectangle.portrait",
+                                   customImageName: "network",
                                    labelText: "NIP-0034 (1633)")
 
                     ListOptionView(destinationView: AnyView(_1632QueryRelayDemoView()),
-                                   imageName: "list.bullet.rectangle.portrait",
+                                   customImageName: "network",
                                    labelText: "NIP-0034 (1632)")
 
                     ListOptionView(destinationView: AnyView(_1631QueryRelayDemoView()),
-                                   imageName: "list.bullet.rectangle.portrait",
+                                   customImageName: "network",
                                    labelText: "NIP-0034 (1631)")
 
                     ListOptionView(destinationView: AnyView(_1630QueryRelayDemoView()),
-                                   imageName: "list.bullet.rectangle.portrait",
+                                   customImageName: "network",
                                    labelText: "NIP-0034 (1630)")
 
                     ListOptionView(destinationView: AnyView(_1621QueryRelayDemoView()),
-                                   imageName: "list.bullet.rectangle.portrait",
+                                   customImageName: "network",
                                    labelText: "NIP-0034 (1621)")
 
                     ListOptionView(destinationView: AnyView(_1617QueryRelayDemoView()),
-                                   imageName: "list.bullet.rectangle.portrait",
+                                   customImageName: "network",
                                    labelText: "NIP-0034 (1617)")
 
                     ListOptionView(destinationView: AnyView(_1632QueryRelayDemoView()),
-                                   imageName: "list.bullet.rectangle.portrait",
+                                   customImageName: "network",
                                    labelText: "NIP-0034 (1632)")
 
                     ListOptionView(destinationView: AnyView(ConnectRelayView(relay: $relay)),
-                                   imageName: "network",
+                                   customImageName: "network",
                                    labelText: "Connect Relay")
+
                     ListOptionView(destinationView: AnyView(RelaysView()),
-                                   imageName: "network",
+                                   customImageName: "network",
                                    labelText: "Configure Relays")
+
                     ListOptionView(destinationView: AnyView(QueryRelayDemoView()),
-                                   imageName: "list.bullet.rectangle.portrait",
+                                   customImageName: "network",
                                    labelText: "NIP-0034 Viewer")
+
                     ListOptionView(destinationView:
                                     AnyView(LegacyDirectMessageDemoView()),
-                                   imageName: "list.bullet",
+                                   customImageName: "network",
                                    labelText: "NIP-04 Direct Message")
+
                     ListOptionView(destinationView:
                                     AnyView(EncryptMessageDemoView()),
-                                   imageName: "list.bullet",
+                                   customImageName: "network",
                                    labelText: "NIP-44 Encrypt")
+
                     ListOptionView(destinationView:
                                     AnyView(DecryptMessageDemoView()),
-                                   imageName: "list.bullet",
+                                   customImageName: "network",
                                    labelText: "NIP-44 Decrypt")
+
                     ListOptionView(destinationView: AnyView(GenerateKeyDemoView()),
-                                   imageName: "key",
+                                   customImageName: "key",
                                    labelText: "Key Generation")
+
                     ListOptionView(destinationView: AnyView(NIP05VerficationDemoView()),
-                                   imageName: "checkmark.seal",
+                                   customImageName: "checkmark.seal",
                                    labelText: "NIP-05")
                 }
             }
@@ -86,7 +98,6 @@ struct ContentView: View {
             .navigationBarTitleDisplayMode(.inline)
         }
     }
-
 }
 
 struct ContentView_Previews: PreviewProvider {
