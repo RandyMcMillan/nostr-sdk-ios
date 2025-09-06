4e8f1f0ea2c644540b20f3859eb3bdfe14986a54
randymcmillan <randymcmillan@protonmail.com>
2025-09-06 08:44:15 -0400

    demo/Package.swift:target dependencies:fix

--- demo/Package.swift
+++ demo/Package.swift
@@ -1,11 +1,28 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
 let package = Package(
     name: "NostrSDKDemo",
+    platforms: [
+        .macOS(.v12),
+        .iOS(.v15),
+    ],
+    products: [
+        .executable(
+            name: "NostrSDKDemo",
+            targets: ["NostrSDKDemo"]
+        )
+    ],
     dependencies: [
-        .package(path: "../")
+        .package(url: "https://github.com/nostr-sdk/nostr-sdk-ios", .upToNextMajor(from: "0.2.0"))
     ],
     targets: [
-        .target(
+        .executableTarget(
             name: "NostrSDKDemo",
-            dependencies: ["NostrSDK"])
+            dependencies: [
+                .product(name: "NostrSDK", package: "nostr-sdk-ios")
+            ]
+        )
     ]
 )
+
