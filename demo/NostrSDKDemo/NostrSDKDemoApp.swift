2fb9e67a1e9941d8ceb8a540968281445e2a809a
randymcmillan <randymcmillan@protonmail.com>
2026-06-06 14:45:39 -0400

    demo/NostrSDKDemo/NostrSDKDemoApp.swift

--- demo/NostrSDKDemo/NostrSDKDemoApp.swift
+++ demo/NostrSDKDemo/NostrSDKDemoApp.swift
@@ -12,9 +12,8 @@ import SwiftUI
 struct NostrSDKDemoApp: App {
     // swiftlint:disable:next force_try
     @StateObject var relayPool = try! RelayPool(relayURLs: [
-        URL(string: "wss://relay.damus.io")!,
-        URL(string: "wss://relay.snort.social")!,
         URL(string: "wss://nos.lol")!,
+        URL(string: "wss://relay.snort.social")!,
         URL(string: "ws://127.0.0.1:8080")!
     ])
     
