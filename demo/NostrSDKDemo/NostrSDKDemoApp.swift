45979441cf3d1956d3b244d11f833f10a7380df2
randymcmillan <randymcmillan@protonmail.com>
2026-06-10 09:28:45 -0400

    demo/NostrSDKDemo/NostrSDKDemoApp.swift

--- demo/NostrSDKDemo/NostrSDKDemoApp.swift
+++ demo/NostrSDKDemo/NostrSDKDemoApp.swift
@@ -56,6 +56,7 @@ private struct AppBootstrapView: View {
                     repositoryHostStore.attach(appPrimeStore: appPrimeStore)
                 }
                 appPrimeStore.attach(relayPool: relayPool)
+                await repositoryHostStore.bootstrapHostedRepositoryIfNeeded()
             }
     }
 }
